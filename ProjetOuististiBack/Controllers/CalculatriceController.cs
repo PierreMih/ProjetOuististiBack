3481e22774fd194bbaa6dcb2edca5e76e5d67052
p-mih <p-mih@LPC01170>
2023-01-02 16:59:59 +0100

    Main Controller

---
+++ ProjetOuististiBack/Controllers/CalculatriceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetOuististiApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatriceController : ControllerBase
+    {
+        private readonly ILogger<CalculatriceController> _logger;
+
+        public CalculatriceController(ILogger<CalculatriceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "Calculate")]
+        public float ReceiveCalculAndReturnResult(CalculDto calcul)
+        {
+            return 10;
+        }
+    }
+}
---
+++ ProjetOuististiModel/Shared/OperationTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetOuististiDomain.Shared
+{
+    public enum OperationTypes
+    {
+        Addition,
+        Soustraction,
+        Multiplication,
+        Division
+    }
+}
