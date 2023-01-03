0119e1cf85699f80621a12ae16d662875c46ee9d
p-mih <p-mih@LPC01170>
2023-01-03 15:33:11 +0100

    Database

---
+++ ProjetOuististiEntityFramework/Migrations/20230103143240_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetOuististiEntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calculs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Input1 = table.Column<float>(type: "REAL", nullable: false),
+                    Input2 = table.Column<float>(type: "REAL", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calculs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calculs");
+        }
+    }
+}
---
+++ ProjetOuististiEntityFramework/ProjetOuististiDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetOuististiDomain.Calcul;
+using ProjetOuististiDomain.Shared;
+
+namespace ProjetOuististiEntityFramework
+{
+    public class ProjetOuististiDbContext : DbContext
+    {
+        public DbSet<CalculAbstract> Calculs { get; set; }
+        public DbSet<CalculAddition> CalculAdditions { get; set; }
+        public DbSet<CalculSoustraction> CalculSoustractions { get; set; }
+        public DbSet<CalculMultiplication> CalculMultiplications { get; set; }
+        public DbSet<CalculDivision> calculDivisions { get; set; }
+
+        public string DbPath { get; }
+
+        public ProjetOuististiDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "ProjetOuististiOuististi.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
