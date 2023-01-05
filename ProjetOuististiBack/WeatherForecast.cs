ba0c4ddd1423b680b096962cfeb59a2bd6898d8a
p-mih <p-mih@LPC01170>
2023-01-05 08:49:45 +0100

    test sonar 2

--- ProjetOuististiBack/WeatherForecast.cs
+++ ProjetOuististiBack/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace ProjetOuististiApplication
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-        //testt
+        //testtt
         public string? Summary { get; set; }
     }
 }
