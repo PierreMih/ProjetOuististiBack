9ea92ea9f36cede3a60d836e7314ffab4d77b7f3
KREMER Hugo <33060853+Metzill@users.noreply.github.com>
2023-01-06 10:29:19 +0100

    Factory Test implementation

---
+++ ProjetOuististiBackTests/Factory/FactoryTest.cs
@@ -0,0 +1,48 @@
+﻿using ProjetOuististiDomain.Calculs;
+using ProjetOuististiDomain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetOuististiBackTests.Factory
+{
+    [TestClass]
+    public class FactoryTest
+    {
+        private readonly float _1 = 41;
+        private readonly float _2 = 3;
+
+        [TestMethod]
+        public void IsOperationTypeNumExceptionThrownWhenWrongOpreationTypeIsPassed()
+        {
+            Assert.ThrowsException<OperationTypeEnumException>(() => CalculFactory.BuildFromValuesAndOpType(_1, _2, "NotAnOperation"));
+        }
+
+        [TestMethod]
+        public void IsAdditionCorrectlyImplementatedThroughFactory()
+        {
+            Assert.IsTrue(CalculFactory.BuildFromValuesAndOpType(_1, _2, "Addition").GetType() == typeof(CalculAddition));
+        }
+
+        [TestMethod]
+        public void IsSubstractionCorrectlyImplementatedThroughFactory()
+        {
+            Assert.IsTrue(CalculFactory.BuildFromValuesAndOpType(_1, _2, "Soustraction").GetType() == typeof(CalculSoustraction));
+        }
+
+        [TestMethod]
+        public void IsMultiplicationCorrectlyImplementatedThroughFactory()
+        {
+            Assert.IsTrue(CalculFactory.BuildFromValuesAndOpType(_1, _2, "Multiplication").GetType() == typeof(CalculMultiplication));
+        }
+
+        [TestMethod]
+        public void IsDivisionCorrectlyImplementatedThroughFactory()
+        {
+            Assert.IsTrue(CalculFactory.BuildFromValuesAndOpType(_1, _2, "Division").GetType() == typeof(CalculDivision));
+        }
+    }
+}
